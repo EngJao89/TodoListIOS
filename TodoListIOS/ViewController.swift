b586c9af1bf9c621db761f2ec82898b4c7c1a8bb
EngJao89 <eng_joao@outlook.com>
2024-11-19 16:14:15 -0300

    Initial Commit

---
+++ TodoListIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListIOS
+//
+//  Created by João Ricardo Martins Ribeiro on 19/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
